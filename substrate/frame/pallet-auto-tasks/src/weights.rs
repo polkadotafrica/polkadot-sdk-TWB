291e7c73bf9ad01fe57b812fb51137254c230e98
Samuel Edu <edubsam@gmail.com>
2025-05-07 09:13:18 +0100

    Fix extra blank lines in weights.rs

--- substrate/frame/pallet-auto-tasks/src/weights.rs
+++ substrate/frame/pallet-auto-tasks/src/weights.rs
@@ -1,21 +1,21 @@
 use frame_support::weights::Weight;
 
 pub trait WeightInfo {
-	fn add_number_into_total() -> Weight;
-	fn store_number() -> Weight;
-	fn get_totals() -> Weight;
+  fn add_number_into_total() -> Weight;
+  fn store_number() -> Weight;
+  fn get_totals() -> Weight;
 }
 
 impl WeightInfo for () {
-	fn add_number_into_total() -> Weight {
-		Weight::from_parts(10_000, 0)
-	}
+  fn add_number_into_total() -> Weight {
+	Weight::from_parts(10_000, 0)
+  }
 	
-	fn store_number() -> Weight {
-		Weight::from_parts(5_000, 0)
-	}
+  fn store_number() -> Weight {
+	Weight::from_parts(5_000, 0)
+  }
 	
-	fn get_totals() -> Weight {
-		Weight::from_parts(2_000, 0)
-	}
+  fn get_totals() -> Weight {
+	Weight::from_parts(2_000, 0)
+  }
 }
